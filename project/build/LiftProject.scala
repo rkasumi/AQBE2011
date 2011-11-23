90aedac5a6d9ac29b92a55482036598b0804a805
rkasumi <r.kasumi@gmail.com>
2011-11-23 18:18:18 +0900

    eclipse 対応

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,6 +1,7 @@
 import sbt._
+import de.element34.sbteclipsify._
 
-class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
+class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) with Eclipsify {
   val liftVersion = property[Version]
 
   // uncomment the following if you want to use the snapshot repo
